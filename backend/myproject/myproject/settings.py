525f9950f93ab600d40672b4f41b9e49a09113e7
Abhinav <abnvv07@gmail.com>
2026-02-26 16:33:06 +0530

    new

--- backend/myproject/myproject/settings.py
+++ backend/myproject/myproject/settings.py
@@ -117,6 +117,8 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 #         'PORT': '5432',
 #     }
 # }
+from decouple import config
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -124,10 +126,9 @@ DATABASES = {
         'USER': config('POSTGRES_USER'),
         'PASSWORD': config('POSTGRES_PASSWORD'),
         'HOST': config('POSTGRES_HOST'),
-        'PORT': config('POSTGRES_PORT', default='5432'),
+        'PORT': config('POSTGRES_PORT', default=5432),
     }
 }
-
 
 
 
