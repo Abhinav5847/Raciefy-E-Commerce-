c944caf722ea40a0ab29abf827dd52a7c12d57a7
Abhinav <abnvv07@gmail.com>
2026-02-26 15:36:56 +0530

    boom

--- backend/myproject/myproject/settings.py
+++ backend/myproject/myproject/settings.py
@@ -75,6 +75,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
@@ -254,10 +255,10 @@ SPECTACULAR_SETTINGS = {
     },
 }
 
+import os
+
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [] 
-
-
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
